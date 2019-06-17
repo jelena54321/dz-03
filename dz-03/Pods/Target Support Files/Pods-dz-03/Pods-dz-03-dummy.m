2fd6478383f9d8272dddaab191f7e5e6a462d72a
Jelena Šarić <jelena.saric@fer.hr>
2019-06-17 02:55:28 +0200

    dodano rjesenje 3. domace zadace

---
+++ dz-03/Pods/Target Support Files/Pods-dz-03/Pods-dz-03-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_dz_03 : NSObject
+@end
+@implementation PodsDummy_Pods_dz_03
+@end
---
+++ dz-03/dz-03/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  dz-03
+//
+//  Created by Jelena Šarić on 11/06/2019.
+//  Copyright © 2019 Jelena Šarić. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if UserDefaults.standard.string(forKey: "token") != nil {
+            window?.rootViewController = TabBarViewController()
+        } else {
+            window?.rootViewController = LoginViewController()
+        }
+        
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
